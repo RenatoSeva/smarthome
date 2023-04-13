0c77581cb7a4b5b163c5c3a483e0d3b2ecdaf38b
vk2308 <130595815+vk2308@users.noreply.github.com>
2023-04-13 18:47:44 +0200

    Update toggle_light.py

--- toggle_light.py
+++ toggle_light.py
@@ -1,9 +1,10 @@
-def toggle_light(room, light_statuses, light_buttons, light_status_label):
-    light_status = light_statuses[room]
-    if light_status:
-        light_buttons[room].config(bg="red")
-        light_status_label[room].config(text=f"{room.capitalize()} Light: OFF")
-    else:
-        light_buttons[room].config(bg="green")
-        light_status_label[room].config(text=f"{room.capitalize()} Light: ON")
-    light_statuses[room] = not light_status
+def toggle_light(room, light_statuses, light_buttons, light_status_label, automation_enabled):
+    if not automation_enabled.get():
+        light_status = light_statuses[room]
+        if light_status:
+            light_buttons[room].config(bg="red")
+            light_status_label[room].config(text=f"{room.capitalize()} Light: OFF")
+        else:
+            light_buttons[room].config(bg="green")
+            light_status_label[room].config(text=f"{room.capitalize()} Light: ON")
+        light_statuses[room] = not light_status
